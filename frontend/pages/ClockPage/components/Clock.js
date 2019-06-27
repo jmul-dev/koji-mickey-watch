09c30291c585f027c867db232d0a684271bcb1e0
Johan <johan@paramation.com>
2019-06-27 09:40:14 +0700

    Fix css

--- frontend/pages/ClockPage/components/Clock.js
+++ frontend/pages/ClockPage/components/Clock.js
@@ -5,245 +5,209 @@ import * as d3 from "d3";
 import "./clock.css";
 
 const Container = styled.div`
-  width: ${({ width }) => width}px;
-  margin: 0 auto;
-  margin-bottom: 24px;
+	width: ${({ width }) => width}px;
+	margin: 0 auto;
+	margin-bottom: 24px;
+`;
+
+const Svg = styled.svg`
+	stroke: ${() => Koji.config.style.textColor};
+	background: url(${() => Koji.config.general.backgroundImage});
+	background-size: 100px;
+	background-position: center;
+	background-repeat: no-repeat;
 `;
 
 class Clock extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    const hourScale = d3
-        .scaleLinear()
-        .range([0, 330])
-        .domain([0, 11]),
-      minuteScale = d3
-        .scaleLinear()
-        .range([0, 354])
-        .domain([0, 59]),
-      secondScale = minuteScale;
+		const hourScale = d3
+				.scaleLinear()
+				.range([0, 330])
+				.domain([0, 11]),
+			minuteScale = d3
+				.scaleLinear()
+				.range([0, 354])
+				.domain([0, 59]),
+			secondScale = minuteScale;
 
-    this.state = {
-      hourScale,
-      minuteScale,
-      secondScale,
-      handData: [
-        {
-          type: "hour",
-          value: 0,
-          length: 0,
-          scale: hourScale
-        },
-        {
-          type: "minute",
-          value: 0,
-          length: 0,
-          scale: minuteScale
-        },
-        {
-          type: "second",
-          value: 0,
-          length: 0,
-          scale: secondScale,
-          balance: 30
-        }
-      ],
-      intervalId: null
-    };
-    this.updateData = this.updateData.bind(this);
-    this.drawClock = this.drawClock.bind(this);
-    this.moveHands = this.moveHands.bind(this);
-  }
+		this.state = {
+			hourScale,
+			minuteScale,
+			secondScale,
+			handData: [
+				{
+					type: "hour",
+					value: 0,
+					length: 0,
+					scale: hourScale
+				},
+				{
+					type: "minute",
+					value: 0,
+					length: 0,
+					scale: minuteScale
+				},
+				{
+					type: "second",
+					value: 0,
+					length: 0,
+					scale: secondScale,
+					balance: 30
+				}
+			],
+			intervalId: null
+		};
+		this.updateData = this.updateData.bind(this);
+		this.drawClock = this.drawClock.bind(this);
+		this.moveHands = this.moveHands.bind(this);
+	}
 
-  componentDidMount() {
-    this.drawClock();
-    const intervalId = setInterval(() => {
-      this.updateData();
-      this.moveHands();
-    }, 1000);
-    this.setState({ intervalId });
-  }
+	componentDidMount() {
+		this.drawClock();
+		const intervalId = setInterval(() => {
+			this.updateData();
+			this.moveHands();
+		}, 1000);
+		this.setState({ intervalId });
+	}
 
-  componentWillUnmount() {
-    clearInterval(this.state.intervalId);
-  }
+	componentWillUnmount() {
+		clearInterval(this.state.intervalId);
+	}
 
-  updateData() {
-    const t = new Date();
-    const { handData } = this.state;
-    handData[0].value = (t.getHours() % 12) + t.getMinutes() / 60;
-    handData[1].value = t.getMinutes();
-    handData[2].value = t.getSeconds();
-    this.setState({ handData });
-  }
+	updateData() {
+		const t = new Date();
+		const { handData } = this.state;
+		handData[0].value = (t.getHours() % 12) + t.getMinutes() / 60;
+		handData[1].value = t.getMinutes();
+		handData[2].value = t.getSeconds();
+		this.setState({ handData });
+	}
 
-  moveHands() {
-    console.log("move hands");
-    const { handData } = this.state;
-    d3.select("#clock-hands")
-      .selectAll("line")
-      .data(handData)
-      .transition()
-      .attr("transform", d => {
-        return "rotate(" + d.scale(d.value) + ")";
-      });
-  }
+	moveHands() {
+		const { handData } = this.state;
+		d3.select("#clock-hands")
+			.selectAll("line")
+			.data(handData)
+			.transition()
+			.attr("transform", (d) => "rotate(" + d.scale(d.value) + ")");
+	}
 
-  drawClock() {
-    //create all the clock elements
-    this.updateData(); //draw them in the correct starting position
+	drawClock() {
+		//create all the clock elements
+		this.updateData(); //draw them in the correct starting position
 
-    const { clockRadius, margin } = this.props;
+		const { clockRadius, margin } = this.props;
 
-    const radians = 0.0174532925,
-      width = (clockRadius + margin) * 2,
-      height = (clockRadius + margin) * 2,
-      hourHandLength = (2 * clockRadius) / 3,
-      minuteHandLength = clockRadius,
-      secondHandLength = clockRadius - 12,
-      secondHandBalance = 30,
-      secondTickStart = clockRadius,
-      secondTickLength = -10,
-      hourTickStart = clockRadius,
-      hourTickLength = -18,
-      secondLabelRadius = clockRadius + 16,
-      secondLabelYOffset = 5,
-      hourLabelRadius = clockRadius - 40,
-      hourLabelYOffset = 7;
+		const radians = 0.0174532925,
+			width = (clockRadius + margin) * 2,
+			height = (clockRadius + margin) * 2,
+			hourHandLength = (2 * clockRadius) / 3,
+			minuteHandLength = clockRadius,
+			secondHandLength = clockRadius - 12,
+			secondHandBalance = 30,
+			secondTickStart = clockRadius,
+			secondTickLength = -6,
+			hourTickStart = clockRadius,
+			hourTickLength = -15,
+			secondLabelRadius = clockRadius + 16,
+			secondLabelYOffset = 5,
+			hourLabelRadius = clockRadius - 40,
+			hourLabelYOffset = 7;
 
-    const { hourScale, minuteScale, secondScale, handData } = this.state;
+		const { hourScale, minuteScale, secondScale, handData } = this.state;
 
-    handData[0].length = -1 * hourHandLength;
-    handData[1].length = -1 * minuteHandLength;
-    handData[2].length = -1 * secondHandLength;
+		handData[0].length = -1 * hourHandLength;
+		handData[1].length = -1 * minuteHandLength;
+		handData[2].length = -1 * secondHandLength;
 
-    const svg = d3
-      .select("svg#clock")
-      .attr("width", width)
-      .attr("height", height);
+		const svg = d3
+			.select("svg#clock")
+			.attr("width", width)
+			.attr("height", height);
 
-    const face = svg
-      .append("g")
-      .attr("id", "clock-face")
-      .attr(
-        "transform",
-        "translate(" +
-          (clockRadius + margin) +
-          "," +
-          (clockRadius + margin) +
-          ")"
-      );
+		const face = svg
+			.append("g")
+			.attr("id", "clock-face")
+			.attr("transform", "translate(" + (clockRadius + margin) + "," + (clockRadius + margin) + ")");
 
-    // add marks for seconds
-    face
-      .selectAll(".second-tick")
-      .data(d3.range(0, 60))
-      .enter()
-      .append("line")
-      .attr("class", "second-tick")
-      .attr("x1", 0)
-      .attr("x2", 0)
-      .attr("y1", secondTickStart)
-      .attr("y2", secondTickStart + secondTickLength)
-      .attr("transform", d => {
-        return "rotate(" + secondScale(d) + ")";
-      });
-    //and labels
+		// add marks for seconds
+		face.selectAll(".second-tick")
+			.data(d3.range(0, 60))
+			.enter()
+			.append("line")
+			.attr("class", "second-tick")
+			.attr("x1", 0)
+			.attr("x2", 0)
+			.attr("y1", secondTickStart)
+			.attr("y2", secondTickStart + secondTickLength)
+			.attr("transform", (d) => "rotate(" + secondScale(d) + ")");
+		//and labels
 
-    face
-      .selectAll(".second-label")
-      .data(d3.range(5, 61, 5))
-      .enter()
-      .append("text")
-      .attr("class", "second-label")
-      .attr("text-anchor", "middle")
-      .attr("x", d => {
-        return secondLabelRadius * Math.sin(secondScale(d) * radians);
-      })
-      .attr("y", d => {
-        return (
-          -secondLabelRadius * Math.cos(secondScale(d) * radians) +
-          secondLabelYOffset
-        );
-      })
-      .text(d => {
-        return d;
-      });
+		face.selectAll(".second-label")
+			.data(d3.range(5, 61, 5))
+			.enter()
+			.append("text")
+			.attr("class", "second-label")
+			.attr("text-anchor", "middle")
+			.attr("x", (d) => secondLabelRadius * Math.sin(secondScale(d) * radians))
+			.attr("y", (d) => -secondLabelRadius * Math.cos(secondScale(d) * radians) + secondLabelYOffset)
+			.text((d) => d);
 
-    //... and hours
-    face
-      .selectAll(".hour-tick")
-      .data(d3.range(0, 12))
-      .enter()
-      .append("line")
-      .attr("class", "hour-tick")
-      .attr("x1", 0)
-      .attr("x2", 0)
-      .attr("y1", hourTickStart)
-      .attr("y2", hourTickStart + hourTickLength)
-      .attr("transform", d => {
-        return "rotate(" + hourScale(d) + ")";
-      });
+		//... and hours
+		face.selectAll(".hour-tick")
+			.data(d3.range(0, 12))
+			.enter()
+			.append("line")
+			.attr("class", "hour-tick")
+			.attr("x1", 0)
+			.attr("x2", 0)
+			.attr("y1", hourTickStart)
+			.attr("y2", hourTickStart + hourTickLength)
+			.attr("transform", (d) => "rotate(" + hourScale(d) + ")");
 
-    face
-      .selectAll(".hour-label")
-      .data(d3.range(3, 13, 3))
-      .enter()
-      .append("text")
-      .attr("class", "hour-label")
-      .attr("text-anchor", "middle")
-      .attr("x", d => {
-        return hourLabelRadius * Math.sin(hourScale(d) * radians);
-      })
-      .attr("y", d => {
-        return (
-          -hourLabelRadius * Math.cos(hourScale(d) * radians) + hourLabelYOffset
-        );
-      })
-      .text(d => {
-        return d;
-      });
+		face.selectAll(".hour-label")
+			.data(d3.range(3, 13, 3))
+			.enter()
+			.append("text")
+			.attr("class", "hour-label")
+			.attr("text-anchor", "middle")
+			.attr("x", (d) => hourLabelRadius * Math.sin(hourScale(d) * radians))
+			.attr("y", (d) => -hourLabelRadius * Math.cos(hourScale(d) * radians) + hourLabelYOffset)
+			.text((d) => d);
 
-    const hands = face.append("g").attr("id", "clock-hands");
+		const hands = face.append("g").attr("id", "clock-hands");
 
-    face
-      .append("g")
-      .attr("id", "face-overlay")
-      .append("circle")
-      .attr("class", "hands-cover")
-      .attr("x", 0)
-      .attr("y", 0)
-      .attr("r", clockRadius / 20);
+		face.append("g")
+			.attr("id", "face-overlay")
+			.append("circle")
+			.attr("class", "hands-cover")
+			.attr("x", 0)
+			.attr("y", 0)
+			.attr("r", clockRadius / 20);
 
-    hands
-      .selectAll("line")
-      .data(handData)
-      .enter()
-      .append("line")
-      .attr("class", d => {
-        return d.type + "-hand";
-      })
-      .attr("x1", 0)
-      .attr("y1", d => {
-        return d.balance ? d.balance : 0;
-      })
-      .attr("x2", 0)
-      .attr("y2", d => {
-        return d.length;
-      })
-      .attr("transform", d => {
-        return "rotate(" + d.scale(d.value) + ")";
-      });
-  }
+		hands
+			.selectAll("line")
+			.data(handData)
+			.enter()
+			.append("line")
+			.attr("class", (d) => d.type + "-hand")
+			.attr("x1", 0)
+			.attr("y1", (d) => (d.balance ? d.balance : 0))
+			.attr("x2", 0)
+			.attr("y2", (d) => d.length)
+			.attr("transform", (d) => "rotate(" + d.scale(d.value) + ")");
+	}
 
-  render() {
-    return (
-      <Container width={320}>
-        <svg id="clock" />
-      </Container>
-    );
-  }
+	render() {
+		return (
+			<Container width={320}>
+				<Svg id="clock" />
+			</Container>
+		);
+	}
 }
 
 export default Clock;
--- frontend/pages/ClockPage/index.js
+++ frontend/pages/ClockPage/index.js
@@ -1,40 +1,38 @@
-import React from 'react';
-import styled, { ThemeProvider, keyframes } from 'styled-components';
-import { Helmet } from 'react-helmet';
-import { FaVolumeUp, FaVolumeMute } from 'react-icons/fa';
-import Koji from 'koji-tools';
+import React from "react";
+import styled, { ThemeProvider, keyframes } from "styled-components";
+import { Helmet } from "react-helmet";
+import { FaVolumeUp, FaVolumeMute } from "react-icons/fa";
+import Koji from "koji-tools";
 
-import Clock from './components/Clock';
+import Clock from "./components/Clock";
 
 const Container = styled.div`
-    display: flex;
-    align-items: center;
-    width: 100%;
-    height: 100vh;
-    background: url(${() => Koji.config.general.backgroundImage});
-    background-size: cover;
-    background-position: center;
-    flex-direction: column;
-    text-align:center;
+	display: flex;
+	align-items: center;
+	width: 100%;
+	height: 100vh;
+	background-color: ${() => Koji.config.style.backgroundColor};
+	flex-direction: column;
+	text-align: center;
 `;
 
 function getFontFamily(ff) {
-    const start = ff.indexOf('family=');
-    if(start === -1) return 'sans-serif';
-    let end = ff.indexOf('&', start);
-    if(end === -1) end = undefined;
-    return ff.slice(start + 7, end);
+	const start = ff.indexOf("family=");
+	if (start === -1) return "sans-serif";
+	let end = ff.indexOf("&", start);
+	if (end === -1) end = undefined;
+	return ff.slice(start + 7, end);
 }
 
 const Cover = styled.div`
-    transition: opacity 0.7s ease-in-out;
-    width: 100%;
-    height: 100vh;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background: linear-gradient(transparent 0, ${({ color }) => color});
-    opacity: ${({ colorSwitch }) => colorSwitch ? 1 : 0};
+	transition: opacity 0.7s ease-in-out;
+	width: 100%;
+	height: 100vh;
+	position: absolute;
+	top: 0;
+	left: 0;
+	background: linear-gradient(transparent 0, ${({ color }) => color});
+	opacity: ${({ colorSwitch }) => (colorSwitch ? 1 : 0)};
 `;
 
 const Content = styled.div`
@@ -52,25 +50,25 @@ const Content = styled.div`
 `;
 
 const VolumeControl = styled.div`
-    position: absolute;
-    top: 6px;
-    right: 6px;
-    font-size: 14px;
-    background-color: rgba(0,0,0,0.6);
-    border-radius: 6px;
-    padding: 6px;
-    display: flex;
-    align-items: center;
-    color: white;
+	position: absolute;
+	top: 6px;
+	right: 6px;
+	font-size: 14px;
+	background-color: rgba(0, 0, 0, 0.6);
+	border-radius: 6px;
+	padding: 6px;
+	display: flex;
+	align-items: center;
+	color: white;
 
-    &:hover {
-        background-color: rgba(0,0,0,0.9);
-    }
+	&:hover {
+		background-color: rgba(0, 0, 0, 0.9);
+	}
 `;
 
 const Title = styled.h1`
-    font-size: 32px;
-    margin: 0;
+	font-size: 32px;
+	margin: 0;
 `;
 
 const Animation = keyframes`
@@ -86,73 +84,74 @@ const Animation = keyframes`
 `;
 
 const Start = styled.button`
-    max-width: 320px;
-    font-size: 24px;
-    background: rgba(0,0,0,0.7);
-    border: 4px solid ${() => Koji.config.style.textColor};
-    color: ${() => Koji.config.style.textColor};
-    box-shadow: 0 2px 12px rgba(0,0,0,0.24);
-    text-shadow: 0 1px 6px rgba(0,0,0,0.4);
-    cursor: pointer;
-    padding: 10px 64px;
-    border-radius: 1000px;
-    margin-bottom: 10vh;
-    animation: ${Animation} 2s ease-in-out infinite;
+	max-width: 320px;
+	font-size: 24px;
+	background: rgba(0, 0, 0, 0.7);
+	border: 4px solid ${() => Koji.config.style.textColor};
+	color: ${() => Koji.config.style.textColor};
+	box-shadow: 0 2px 12px rgba(0, 0, 0, 0.24);
+	text-shadow: 0 1px 6px rgba(0, 0, 0, 0.4);
+	cursor: pointer;
+	padding: 10px 64px;
+	border-radius: 1000px;
+	margin-bottom: 10vh;
+	animation: ${Animation} 2s ease-in-out infinite;
 
-    transition: background-color 0.1s ease-in-out;
+	transition: background-color 0.1s ease-in-out;
 
-    &:hover {
-        background-color: rgba(0,0,0,0.3);
-    }
+	&:hover {
+		background-color: rgba(0, 0, 0, 0.3);
+	}
 `;
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            color: '#fff',
-            start: false,
-            muted: localStorage.getItem('muted') === 'true' || false,
-        };
-    }
+		this.state = {
+			color: "#fff",
+			start: false,
+			muted: localStorage.getItem("muted") === "true" || false
+		};
+	}
 
-    componentDidMount() {
-        Koji.on('change', () => {
-            this.forceUpdate();
-        })
-    }
+	componentDidMount() {
+		Koji.on("change", () => {
+			this.forceUpdate();
+		});
+	}
 
-    flash(color) {
-        this.setState({ color, colorSwitch: true });
-        setTimeout(() => this.setState({ colorSwitch: false }), 700);
-    }
+	flash(color) {
+		this.setState({ color, colorSwitch: true });
+		setTimeout(() => this.setState({ colorSwitch: false }), 700);
+	}
 
-    toggleMute(muted) {
-        this.setState({ muted });
-        localStorage.setItem('muted', muted);
-    }
+	toggleMute(muted) {
+		this.setState({ muted });
+		localStorage.setItem("muted", muted);
+	}
 
-  render() {
-    return (
-        <Container color={this.state.color}>
-            <Helmet defaultTitle={Koji.config.general.name}>
-                <link href={Koji.config.general.fontFamily} rel="stylesheet" />
-            </Helmet>
-            <Cover color={this.state.color} colorSwitch={this.state.colorSwitch} />
-            <Content>
-                <VolumeControl>
-                    {this.state.muted ? 
-                        <FaVolumeMute onClick={() => this.toggleMute(false)} />
-                      : <FaVolumeUp onClick={() => this.toggleMute(true)} />
-                    }
-                </VolumeControl>
-                <Title>{Koji.config.general.name}</Title>
-				<Clock clockRadius={120} margin={40} muted={this.state.muted} />
-            </Content>
-        </Container>
-    );
-  }
+	render() {
+		return (
+			<Container color={this.state.color}>
+				<Helmet defaultTitle={Koji.config.general.name}>
+					<link href={Koji.config.general.fontFamily} rel="stylesheet" />
+				</Helmet>
+				<Cover color={this.state.color} colorSwitch={this.state.colorSwitch} />
+				<Content>
+					<VolumeControl>
+						{this.state.muted ? (
+							<FaVolumeMute onClick={() => this.toggleMute(false)} />
+						) : (
+							<FaVolumeUp onClick={() => this.toggleMute(true)} />
+						)}
+					</VolumeControl>
+					<Title>{Koji.config.general.name}</Title>
+					<Clock clockRadius={120} margin={40} muted={this.state.muted} />
+				</Content>
+			</Container>
+		);
+	}
 }
 
 export default HomePage;
